712af3c31c3c1c0d693d131e53c49882427ee687
can gozpinar <cgozpinar18@ku.edu.tr>
2020-12-13 20:33:15 +0300

    teco merge

---
+++ src/com/company/Utils/CenterWindow.java
@@ -0,0 +1,14 @@
+package com.company.Utils;
+
+import java.awt.*;
+
+public class CenterWindow {
+
+    public static void centerWindow(Window frame) {
+        Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+        int x = (int) ((dimension.getWidth() - frame.getWidth()) / 2);
+        int y = (int) ((dimension.getHeight() - frame.getHeight()) / 2);
+        frame.setLocation(x, y);
+    }
+
+}
