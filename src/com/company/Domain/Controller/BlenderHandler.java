f605d4825d2e10f242dde614ad41b27757c17432
can gozpinar <cgozpinar18@ku.edu.tr>
2020-12-13 20:32:16 +0300

    teco merging

--- src/com/company/Domain/Controller/BlenderHandler.java
+++ src/com/company/Domain/Controller/BlenderHandler.java
@@ -2,11 +2,13 @@ package com.company.Domain.Controller;
 
 import com.company.Domain.Models.Blender;
 import com.company.Enums.AtomType;
+import com.company.UI.Objects.BlenderWindowFactory;
+import com.company.UI.Objects.GameWindowFactory;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-public class BlenderHandler  {
+public class BlenderHandler implements KeyListener {
     private Blender blender;
     public BlenderHandler() {
         this.blender = Blender.getInstance();
@@ -18,5 +20,21 @@ public class BlenderHandler  {
         blender.blendAtoms(source, product, sourceAmount);
     }
 
+    @Override
+    public void keyTyped(KeyEvent e) {
 
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if(e.getKeyCode() == 66){ //B
+            BlenderWindowFactory blenderFactory = new BlenderWindowFactory();
+            blenderFactory.render();
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
 }
