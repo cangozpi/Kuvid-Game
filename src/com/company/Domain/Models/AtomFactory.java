4753bb504c75d91f8d4da3ad0b71a1b0f8a05b3c
can gozpinar <cgozpinar18@ku.edu.tr>
2020-12-23 01:04:23 +0300

    modified AtomFactory to implement decorator classes for new Atom

--- src/com/company/Domain/Models/AtomFactory.java
+++ src/com/company/Domain/Models/AtomFactory.java
@@ -1,6 +1,7 @@
 package com.company.Domain.Models;
 
 import com.company.Domain.Models.Projectile.Atom;
+import com.company.Domain.Models.Projectile.Decorator.AlphaDecorator;
 import com.company.Domain.Utility.Coordinate;
 import com.company.Domain.Utility.Velocity;
 import com.company.Enums.AtomType;
@@ -12,15 +13,20 @@ public class AtomFactory {
     }
 
     public Atom getInstance(Coordinate coordinate, Velocity velocity, AtomType atomType, boolean isAmmo, int height, int width){
+        Atom referenceAtom;
         switch (atomType){
             case ALPHA:
-                return new Atom(coordinate, velocity, AtomType.ALPHA, isAmmo, height, width);
+                referenceAtom = new Atom(coordinate, velocity, AtomType.ALPHA, isAmmo, height, width);
+                return new AlphaDecorator(coordinate, velocity, AtomType.ALPHA, isAmmo, height, width, referenceAtom);
             case BETA:
-                return new Atom(coordinate, velocity, AtomType.BETA, isAmmo, height, width);
+                referenceAtom = new Atom(coordinate, velocity, AtomType.BETA, isAmmo, height, width);
+                return new AlphaDecorator(coordinate, velocity, AtomType.BETA, isAmmo, height, width, referenceAtom);
             case GAMMA:
-                return new Atom(coordinate, velocity, AtomType.GAMMA, isAmmo, height, width);
-            default:
-                return new Atom(coordinate, velocity, AtomType.SIGMA, isAmmo, height, width);
+                referenceAtom = new Atom(coordinate, velocity, AtomType.GAMMA, isAmmo, height, width);
+                return new AlphaDecorator(coordinate, velocity, AtomType.GAMMA, isAmmo, height, width, referenceAtom);
+            default: //case SIGMA:
+                referenceAtom = new Atom(coordinate, velocity, AtomType.SIGMA, isAmmo, height, width);
+                return new AlphaDecorator(coordinate, velocity, AtomType.SIGMA, isAmmo, height, width, referenceAtom);
         }
     }
 }
