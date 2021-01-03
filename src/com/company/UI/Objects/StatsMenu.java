6459836f4f3249a37256762d3dc7435510ffbaf3
SAKIZ <osakiz18@ku.edu.tr>
2021-01-03 16:11:00 +0300

    Oykum modified Stats Panel.

--- src/com/company/UI/Objects/StatsMenu.java
+++ src/com/company/UI/Objects/StatsMenu.java
@@ -22,31 +22,34 @@ public class StatsMenu {
         JFrame frame = new JFrame("SideBar");
         JPanel panel = new JPanel();
         panel.setBounds(40,80,248,480);
-        panel.setBackground(Color.CYAN);
-        panel.setOpaque(true);
+        panel.setBackground(Color.BLACK);
+        panel.setOpaque(false);
         panel.setLayout(new BoxLayout(panel,BoxLayout.Y_AXIS));
-       // BufferedImage clock = ImageIO.read(new File("./PerfectedHeart.jpg"));
-        JLabel score = new JLabel("Score :");//getScore//
-       // JLabel health = new JLabel("Health Points : ");//health//
-        JButton eta=new JButton("Eta Shield");
-        eta.setBackground(Color.yellow);
-        JButton lota=new JButton("Lota Shield");
-        lota.setBackground(Color.green);
-        JButton theta = new JButton("Theta Shield");
-        theta.setBackground(Color.red);
-        JButton zeta = new JButton("Zeta Shield");
-        zeta.setBackground(Color.WHITE);
-        //Öyküm
 
+        JLabel score = new JLabel("Score :");//getScore//
+
+        score.setForeground(Color.black);
+
+        JButton eta=new JButton("Eta Shield");
+        eta.setForeground(Color.BLUE);
+        JButton lota=new JButton("Lota Shield");
+        lota.setForeground(Color.BLUE);
+
+        JButton theta = new JButton("Theta Shield");
+        theta.setForeground(Color.BLUE);
+        JButton zeta = new JButton("Zeta Shield");
+        zeta.setForeground(Color.BLUE);
+
+//panel bounds (unused)
         //panel.setBounds(windowWidth,-10, windowWidthExtended-windowWidth, windowHeight);
 
 
-
-      //JLabel score = new JLabel("Score: " + String.valueOf(gameFactory.getScore()) );
+// score
+        //JLabel score = new JLabel("Score: " + String.valueOf(gameFactory.getScore()) );
         //score.setLocation(0,0);
-       //panel.add(score);
+        //panel.add(score);
 
-
+// clock
         // ImageIcon clock = new ImageIcon(getClass().getResource(""));
         // Image clockscaled = clock.getImage().getScaledInstance(30, 30, java.awt.Image.SCALE_SMOOTH);
         // JLabel time = new JLabel(new ImageIcon(clockscaled));
@@ -55,7 +58,7 @@ public class StatsMenu {
         //timeLabel.setLocation(2,2);
         ////panel.add(timeLabel);
 
-
+//health
         // ImageIcon healthIcon = new ImageIcon(getClass().getResource(""));
         // Image healthscaled = clock.getImage().getScaledInstance(30, 30, java.awt.Image.SCALE_SMOOTH);
         //  JLabel health = new JLabel(new ImageIcon(healthscaled));
@@ -63,67 +66,68 @@ public class StatsMenu {
         //  JLabel healthLabel = new JLabel(String.valueOf());
         //  Score.add(healthLabel);
         //
-
+//powerups
         ImageIcon alphapowerup = new ImageIcon(getClass().getResource("Assets/powerups/+alpha-b.png"));
         Image alphapow = alphapowerup.getImage().getScaledInstance(30, 30, java.awt.Image.SCALE_SMOOTH);
         JLabel powalpha = new JLabel(new ImageIcon(alphapow));
         //powalpha.setLocation(6,0);
-        panel.add(powalpha);
+
         //JLabel powAlphaAmountLabel = new JLabel(String.valueOf(inventory.getPowerUpAmount(PowerUpType.ALPHA)));
         //powAlphaAmountLabel.setLocation(6,2);
         //panel.add(powAlphaAmountLabel);
 
-        // ImageIcon betapowerup = new ImageIcon(getClass().getResource("Objects/Assets/powerups/beta.png"));
-        // Image betapow = alphapowerup.getImage().getScaledInstance(30, 30, java.awt.Image.SCALE_SMOOTH);
-        // JLabel powbeta = new JLabel(new ImageIcon(betapow));
+         ImageIcon betapowerup = new ImageIcon(getClass().getResource("Assets/powerups/+beta-b.png"));
+         Image betapow = betapowerup.getImage().getScaledInstance(30, 30, java.awt.Image.SCALE_SMOOTH);
+         JLabel powbeta = new JLabel(new ImageIcon(betapow));
         //powbeta.setLocation(8,0);
         // panel.add(powbeta);
         // JLabel powBetaAmountLabel = new JLabel(String.valueOf(inventory.getPowerUpAmount(PowerUpType.BETA)));
         //powBetaAmountLabel.setLocation(8,2);
         // panel.add(powAlphaAmountLabel);
 
-        // ImageIcon gammepowerup = new ImageIcon(getClass().getResource("Objects/Assets/powerups/gamma.png"));
-        // Image gammapow = alphapowerup.getImage().getScaledInstance(30, 30, java.awt.Image.SCALE_SMOOTH);
-        //JLabel powgamma = new JLabel(new ImageIcon(gammapow));
+         ImageIcon gammapowerup = new ImageIcon(getClass().getResource("Assets/powerups/+gamma-b.png"));
+         Image gammapow = gammapowerup.getImage().getScaledInstance(30, 30, java.awt.Image.SCALE_SMOOTH);
+         JLabel powgamma = new JLabel(new ImageIcon(gammapow));
         //powgamma.setLocation(10,0);
         //panel.add(powalpha);
         // JLabel powGammaAmountLabel = new JLabel(String.valueOf(inventory.getPowerUpAmount(PowerUpType.GAMMA)));
         //powGammaAmountLabel.setLocation(10,2);
         // panel.add(powGammaAmountLabel );
 
-        //ImageIcon Sigmapowerup = new ImageIcon(getClass().getResource("Objects/Assets/powerups/sigma.png"));
-        // Image sigmapow = alphapowerup.getImage().getScaledInstance(30, 30, java.awt.Image.SCALE_SMOOTH);
-        // JLabel powsigma = new JLabel(new ImageIcon(sigmapow));
+        ImageIcon Sigmapowerup = new ImageIcon(getClass().getResource("Assets/powerups/+sigma-b.png"));
+         Image sigmapow = Sigmapowerup.getImage().getScaledInstance(30, 30, java.awt.Image.SCALE_SMOOTH);
+         JLabel powsigma = new JLabel(new ImageIcon(sigmapow));
         //powsigma.setLocation(10,0);
         ////panel.add(powsigma);
         // JLabel powSigmaAmountLabel = new JLabel(String.valueOf(inventory.getPowerUpAmount(PowerUpType.SIGMA)));
         //powSigmaAmountLabel.setLocation(10,2);
         // panel.add(powSigmaAmountLabel);
-
-        // ImageIcon mixer = new ImageIcon(getClass().getResource("Objects/Assets/mixer.png"));
-        //Image mixerImage = alphapowerup.getImage().getScaledInstance(30, 30, java.awt.Image.SCALE_SMOOTH);
-        //JLabel mixerLabel = new JLabel(new ImageIcon(mixerImage));
-        //mixerLabel.setLocation(12,1);
+//inventory atom amounts
+        ImageIcon mixer = new ImageIcon(getClass().getResource("Assets/mixer.png"));
+        Image mixerImage = mixer.getImage().getScaledInstance(50, 50, java.awt.Image.SCALE_SMOOTH);
+        JLabel mixerLabel = new JLabel( new ImageIcon(mixerImage));
+        mixerLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+        //mixerLabel.setLocation(12,50);
         //panel.add(mixerLabel);
 
 
-        //ImageIcon alpha = new ImageIcon(getClass().getResource("Objects/Assets/atoms/alpha.png"));
-        //Image scaledAlpha = alpha.getImage().getScaledInstance(30, 30, java.awt.Image.SCALE_SMOOTH); // scales the image
-        //JLabel alphaLabel = new JLabel(new ImageIcon(scaledAlpha));
+        ImageIcon alpha = new ImageIcon(getClass().getResource("Assets/atoms/alpha.png"));
+        Image scaledAlpha = alpha.getImage().getScaledInstance(30, 30, java.awt.Image.SCALE_SMOOTH); // scales the image
+        JLabel alphaLabel = new JLabel(new ImageIcon(scaledAlpha));
         //alphaLabel.setLocation(14,0);
-        // ImageIcon beta = new ImageIcon(getClass().getResource("Objects/Assets/atoms/beta.png"));
-        // Image scaledBeta = beta.getImage().getScaledInstance(30, 30, java.awt.Image.SCALE_SMOOTH); // scales the image
-        //JLabel betaLabel = new JLabel(new ImageIcon(scaledBeta));
+        ImageIcon beta = new ImageIcon(getClass().getResource("Assets/atoms/beta.png"));
+         Image scaledBeta = beta.getImage().getScaledInstance(30, 30, java.awt.Image.SCALE_SMOOTH); // scales the image
+        JLabel betaLabel = new JLabel(new ImageIcon(scaledBeta));
        // betaLabel.setLocation(16,0);
 
-        // ImageIcon gamma = new ImageIcon(getClass().getResource("Objects/Assets/atoms/gamma.png"));
-        //Image scaledGamma = gamma.getImage().getScaledInstance(30, 30, java.awt.Image.SCALE_SMOOTH); // scales the image
-        //JLabel gammaLabel = new JLabel(new ImageIcon(scaledGamma));
+         ImageIcon gamma = new ImageIcon(getClass().getResource("Assets/atoms/gamma.png"));
+        Image scaledGamma = gamma.getImage().getScaledInstance(30, 30, java.awt.Image.SCALE_SMOOTH); // scales the image
+        JLabel gammaLabel = new JLabel(new ImageIcon(scaledGamma));
         //gammaLabel.setLocation(18,0);
 
-        ////ImageIcon sigma = new ImageIcon(getClass().getResource("Objects/Assets/atoms/sigma.png"));
-        //Image scaledSigma = sigma.getImage().getScaledInstance(30, 30, java.awt.Image.SCALE_SMOOTH); // scales the image
-        // JLabel sigmaLabel = new JLabel(new ImageIcon(scaledSigma));
+        ImageIcon sigma = new ImageIcon(getClass().getResource("Assets/atoms/sigma.png"));
+        Image scaledSigma = sigma.getImage().getScaledInstance(30, 30, java.awt.Image.SCALE_SMOOTH); // scales the image
+         JLabel sigmaLabel = new JLabel(new ImageIcon(scaledSigma));
         //sigmaLabel.setLocation(20,0);
 
         //panel.add(alphaLabel);
@@ -145,9 +149,21 @@ public class StatsMenu {
        // JLabel sigmaAmountLabel = new JLabel(String.valueOf(inventory.getAtomAmount(AtomType.SIGMA)));
        // sigmaAmountLabel.setLocation(20,2);
        // panel.add(sigmaAmountLabel);
-
+//panel design
          panel.add(score);
         // panel.add(health);
+        panel.add(new JSeparator(SwingConstants.HORIZONTAL));
+        panel.add(powalpha);
+        panel.add(powbeta);
+        panel.add(powgamma);
+        panel.add(powsigma);
+        panel.add(new JSeparator(SwingConstants.HORIZONTAL));
+        panel.add(mixerLabel);
+        panel.add(alphaLabel);
+        panel.add(betaLabel);
+        panel.add(gammaLabel);
+        panel.add(sigmaLabel);
+        panel.add(new JSeparator(SwingConstants.HORIZONTAL));
         panel.add(eta);
         panel.add(lota);
         panel.add(theta);
