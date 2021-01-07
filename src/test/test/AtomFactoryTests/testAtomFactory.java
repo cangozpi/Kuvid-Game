762744634668bcde1c8e2dbff7bc48302c5aae6c
can gozpinar <cgozpinar18@ku.edu.tr>
2021-01-07 23:14:46 +0300

    @After tearDown() After each call it should return an instance of Atom class if no Exception is raised

--- src/test/test/AtomFactoryTests/testAtomFactory.java
+++ src/test/test/AtomFactoryTests/testAtomFactory.java
@@ -4,10 +4,15 @@ package AtomFactoryTests;
 import com.company.Domain.Models.AtomFactory;
 import com.company.Domain.Models.GameFactory;
 import com.company.Domain.Models.Projectile.Atom;
+import com.company.Domain.Models.Projectile.Decorator.AlphaDecorator;
+import com.company.Domain.Models.Projectile.Decorator.BetaDecorator;
+import com.company.Domain.Models.Projectile.Decorator.GammaDecorator;
+import com.company.Domain.Models.Projectile.Decorator.SigmaDecorator;
 import com.company.Domain.Utility.Coordinate;
 import com.company.Domain.Utility.Velocity;
 import com.company.Enums.AtomType;
 import com.company.Enums.IProjectileType;
+import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -17,6 +22,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 import static org.junit.jupiter.api.Assumptions.assumeFalse;
 
@@ -25,23 +32,46 @@ public class testAtomFactory {
     //NOTE: if tests do not work when you run the program then you have to do File->ProjectStructure->Modules
     // and then change junit.api and j4test to compile to fix the issue. and use java 1.8
 
+    //variable to hold Atom instances that are generated on each test call
+    ArrayList<Atom> initializedAtomInstances = new ArrayList<>();
+
     //denotes that the annotated method will be executed before each test method
+
+
     @BeforeEach
-    //@DisplayName("A special test case")
-    public void beforeClass() throws Exception {
+    void init() {
 
     }
 
-    //denotes that the annotated method will be executed after each test method
-    @AfterEach
-    public void afterClass() throws Exception {
+    //after each test getInstance() should return an instance of Atom class if no Exception is raised
+    @After
+    public void tearDown() throws Exception {
+        initializedAtomInstances.stream().forEach((x) -> {
+            switch(x.getAtomType()){
+                case ALPHA:
+                    assertEquals(x.getClass(), AlphaDecorator.class);
+                    break;
+                case BETA:
+                    assertEquals(x.getClass(), BetaDecorator.class);
+                    break;
+                case SIGMA:
+                    assertEquals(x.getClass(), SigmaDecorator.class);
+                    break;
+                case GAMMA:
+                    assertEquals(x.getClass(), GammaDecorator.class);
+                    break;
+            }
+        });
 
+        //clear the list for saving resources
+        initializedAtomInstances.clear();
     }
 
     //returned atom should be initialized correctly with the supplied parameters
     // (i.e Atom should have the same coordinate, velocity, projectileType, isAmmo, height, width).
     @Test
     public void initializeWithCorrectParameters(){
+        //check btw the passed in parameters and the returned instance's variables
         //valid parameters to initialize Atoms below
         Coordinate[] coordinates = {new Coordinate(0,0), new Coordinate(1,-1), new Coordinate(-2,2), new Coordinate(3,3)};
         Velocity[] velocitys = {new Velocity(10, 1), new Velocity(20, 2), new Velocity(30, 3.3), new Velocity(40, 4.4)};
@@ -49,10 +79,12 @@ public class testAtomFactory {
         boolean[] isAmmos = {true, false, true, false};
         int[] heights = {1, 2, 3, 4};
         int[] widths = {4,3,2,1};
-
-        //check btw the passed in parameters and the returned instance's variables
+        int a = 5;
         for(int i = 0; i < 4; i++){
             Atom testInstance = new AtomFactory().getInstance(coordinates[i], velocitys[i], projectileTypes[i], isAmmos[i], heights[i], widths[i]);
+            //add to list to check whether it stays as valid at the end of the test call
+            initializedAtomInstances.add(testInstance);
+
             //check coordinate
             assertEquals(testInstance.getCoordinate(), coordinates[i]);
             //check velocity
