87254841bbf7c0587768cad54efb635add704e88
can gozpinar <cgozpinar18@ku.edu.tr>
2021-01-07 21:00:00 +0300

    added @effects claus to the AtomFactory

--- src/test/test/AtomFactoryTests/testAtomFactory.java
+++ src/test/test/AtomFactoryTests/testAtomFactory.java
@@ -1,6 +1,7 @@
 package AtomFactoryTests;
 
 
+import com.company.Domain.Models.GameFactory;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -10,9 +11,11 @@ import static org.junit.jupiter.api.Assumptions.assumeFalse;
 
 
 public class testAtomFactory {
+    //note: if tests do not work when you run the program then you have to do File->ProjectStructure->Modules
+    // and then change junit.api and j4test to compile to fix the issue. and use java 1.8
     @Test
     public void name() {
-        assertEquals(5 + 2, 7);
+        assertEquals(GameFactory.class, GameFactory.getInstance().getClass());
         assumeFalse(5 < 1);
 
     }
