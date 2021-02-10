b5e636ee9e116f96339d20b30d01ca44e0fca0c2
can <cangozpinar@gmail.com>
2021-02-11 00:12:24 +0300

    project finalised

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,13 +5,8 @@ import com.company.UI.*;
 public class Main {
 
     public static void main(String[] args) {
-
          //Builder Mode initialization
          BuildWindowFactory buildWindow = BuildWindowFactory.getInstance();
          buildWindow.render();
-
-
-
-
     }
 }
